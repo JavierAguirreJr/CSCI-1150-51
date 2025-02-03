f00fcaf1ad430f15dda1289249344458684fefef
Javier Aguirre <dyenamicempire@gmail.com>
2025-02-03 00:00:22 -0600

    ex 4 and ex 5 done w/image included

--- Unit 1 Solutions/ex04/StopSign.cs
+++ Unit 1 Solutions/ex04/StopSign.cs
@@ -3,8 +3,16 @@ using static System.Console;
 using System.Globalization;
 class StopSign
 {
-	static void Main()
-	{
-		// Write your code here
-	}
+    static void Main()
+    {
+        // Write your code here
+        Console.WriteLine("   XXXXXX   "); //3
+        Console.WriteLine(" X        X "); //9
+        Console.WriteLine("X   STOP   X"); //6
+        Console.WriteLine(" X        X "); //9
+        Console.WriteLine("   XXXXXX   "); //3
+        Console.WriteLine("      X"); //6
+        Console.WriteLine("      X"); //6
+        Console.WriteLine("      X"); //6
+    }
 }
--- Unit 1 Solutions/ex05/BigLetter.cs
+++ Unit 1 Solutions/ex05/BigLetter.cs
@@ -3,8 +3,14 @@ using static System.Console;
 using System.Globalization;
 class BigLetter
 {
-	static void Main()
-	{
-		// Write your code here
-	}
+    static void Main()
+    {
+        // Write your code here
+        Console.WriteLine("H      H");
+        Console.WriteLine("H      H");
+        Console.WriteLine("HHHHHHHH");
+        Console.WriteLine("H      H");
+        Console.WriteLine("H      H");
+        Console.WriteLine("H      H");
+    }
 }
