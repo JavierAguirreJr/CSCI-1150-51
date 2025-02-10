9732303b2d73c1b0814aa40c5d88db0eef1c5dd9
Javier Aguirre <dyenamicempire@gmail.com>
2025-02-10 14:00:05 -0600

    assignments complete

--- Unit 2 Solutions/de02/DebugTwo2.cs
+++ Unit 2 Solutions/de02/DebugTwo2.cs
@@ -4,21 +4,25 @@ using System;
 using static System.Console;
 class DebugTwo2
 {
-   static void main()
-   {
-      string name;
-      string firstString, secondSting;
-      int first, second, product;
-      Write("Enter your name >> );
-      name = ReadLine;
-      Write("Hello, {0}! Enter an integer >> ", name);
-      firstString = ReadLine();
-      first = ConvertToInt32(firstString);
-      Write("Enter another integer >> ");
-      secondString = Readline();
-      second = Convert.ToInt(secondString);
-      product = first * second;
-      WriteLine("Thank you, {1}. The product of {2} and {3} is {4}",
-	 name, first, second, product);
-   }
+    static void main()
+    {
+        string name;
+        string firstString; 
+        string secondString;
+        int first;
+        int second; 
+        int product;
+        Console.WriteLine("Enter your name >> ");
+  
+        name = Console.ReadLine();
+        Write("Hello, {0}! Enter an integer >> ", name);
+        firstString = Console.ReadLine();
+        first = Convert.ToInt32(firstString);
+        Write("Enter another integer >> ");
+        secondString = Console.ReadLine();
+        second = Convert.ToInt32(secondString);
+        product = first * second;
+        WriteLine("Thank you, {1}. The product of {2} and {3} is {4}",
+       name, first, second, product);
+    }
 }
